622bdedbab021ef7c36dad64c5469c12ad25f739
Jorge Casatejada <uo282294@uniovi.es>
2023-02-06 16:51:54 +0100

    SDI-2223-301-1.2-Manejo de sesiones.

---
+++ src/main/java/com/uniovi/es/ServletShoppingCart.java
@@ -0,0 +1,59 @@
+package com.uniovi.es;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@WebServlet(name = "ServletShoppingCart", value = "/AddToShoppingCart")
+public class ServletShoppingCart extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        HashMap<String, Integer> cart =
+                (HashMap<String, Integer>) request.getSession().getAttribute("cart");
+    // No hay carrito, creamos uno y lo insertamos en sesión
+        if (cart == null) {
+            cart = new HashMap<String, Integer>();
+            request.getSession().setAttribute("cart", cart);
+        }
+        String product = request.getParameter("product");
+        if (product != null) {
+            addToShoppingCart(cart, product);
+        }
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<HTML>");
+        out.println("<HEAD><TITLE>Tienda SDI: Cesta de la compra</TITLE></HEAD>");
+        out.println("<BODY>");
+        out.println(shoppingCartToHtml(cart) + "<br>");
+        out.println("<a href=\"shop.html\">Volver</a></BODY></HTML>");
+    }
+
+    private String shoppingCartToHtml(Map<String, Integer> cart) {
+        String shoppingCartToHtml = "";
+
+        for (String key:cart.keySet())
+            shoppingCartToHtml += "<p>[" + key + "], " + cart.get(key) + " unidades</p>";
+        return shoppingCartToHtml;
+    }
+
+    private void addToShoppingCart(Map<String, Integer> cart, String productKey) {
+        if (cart.get(productKey) == null)
+            cart.put(productKey, Integer.valueOf(1));
+        else {
+            int productCount = (Integer) cart.get(productKey).intValue();
+            cart.put(productKey, Integer.valueOf(productCount + 1));
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
