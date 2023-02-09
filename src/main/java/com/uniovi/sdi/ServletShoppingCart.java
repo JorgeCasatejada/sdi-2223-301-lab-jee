c0c8c55835058013b30510bb38285a91816df9e8
Jorge Casatejada <uo282294@uniovi.es>
2023-02-09 16:10:14 +0100

    SDI-2223-301-1.8-MVC, Modelo Vista Controlador.

--- src/main/java/com/uniovi/sdi/ServletShoppingCart.java
+++ src/main/java/com/uniovi/sdi/ServletShoppingCart.java
@@ -25,6 +25,10 @@ public class ServletShoppingCart extends HttpServlet {
         if (product != null) {
             addToShoppingCart(cart, product);
         }
+        request.setAttribute("selectedItems", cart);
+        getServletContext().getRequestDispatcher("/cart.jsp").forward(request, response);
+
+         /* Eliminado al aplicar el patrón MVC
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
@@ -32,7 +36,7 @@ public class ServletShoppingCart extends HttpServlet {
         out.println("<HEAD><TITLE>Tienda SDI: Cesta de la compra</TITLE></HEAD>");
         out.println("<BODY>");
         out.println(shoppingCartToHtml(cart) + "<br>");
-        out.println("<a href=\"index.jsp\">Volver</a></BODY></HTML>");
+        out.println("<a href=\"index.jsp\">Volver</a></BODY></HTML>");*/
     }
 
     private String shoppingCartToHtml(Map<String, Integer> cart) {
